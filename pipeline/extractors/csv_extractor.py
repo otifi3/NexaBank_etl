0ae20858bb28b3aa05cc794e717080af1f8c5aa3
otifi3 <ahmedotifi11112000@gmail.com>
2025-05-11 23:56:01 +0300

    ././.

--- pipeline/extractors/csv_extractor.py
+++ pipeline/extractors/csv_extractor.py
@@ -1,19 +1,19 @@
 import pandas as pd
 
 class CSVExtractor:
-    def __init__(self, file_path: str):
+    def __init__(self):
         """
         Initializes the CSVExtractor with the path to the CSV file.
 
         :param file_path: Path to the CSV file.
         """
 
-    def extract(self) -> pd.DataFrame:
+    def extract(self, file_path: str) -> pd.DataFrame:
         """
         Extracts data from a CSV file and returns it as a pandas DataFrame.
         """
         # Read the CSV file into a DataFrame
-        df = pd.read_csv(self.file_path)
+        df = pd.read_csv(file_path)
         
         # Return the DataFrame
         return df
--- pipeline/extractors/txt_extractor.py
+++ pipeline/extractors/txt_extractor.py
@@ -1,19 +1,19 @@
 import pandas as pd
 
 class TXTExtractor:
-    def __init__(self, file_path: str):
+    def __init__(self):
         """
         Initializes the TXTExtractor with the path to the TXT file.
 
         :param file_path: Path to the TXT file.
         """
 
-    def extract(self, sep) -> pd.DataFrame:
+    def extract(self, sep: str, file_path: str) -> pd.DataFrame:
         """
         Extracts data from a TXT file and returns it as a pandas DataFrame.
         """
         # Read the TXT file into a DataFrame
-        df = pd.read_csv(self.file_path, sep=sep)
+        df = pd.read_csv(file_path, sep=sep)
         
         # Return the DataFrame
         return df
